6b9823d18f32899b0df27312c67ab4ffc3bbe4bb
dong wen cai <mrdong0826@gmail.com>
2018-06-14 15:58:24 +0800

    CStartEventHandler CStopEventHandler

--- src/com/tutorialspoint/MainApp.java
+++ src/com/tutorialspoint/MainApp.java
@@ -1,11 +1,13 @@
 package com.tutorialspoint;
-import org.springframework.context.ApplicationContext;
+import org.springframework.context.ConfigurableApplicationContext;
 import org.springframework.context.support.ClassPathXmlApplicationContext;
 public class MainApp {
     public static void main(String[] args) {
-        ApplicationContext context =
+        ConfigurableApplicationContext  context =
                 new ClassPathXmlApplicationContext("Beans.xml");
+        context.start();
         HelloWorld obj = (HelloWorld) context.getBean("helloWorld");
         obj.getMessage();
+        context.stop();
     }
 }
